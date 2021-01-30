d4096297bfbccdd38ef6e692034a717ae3e1b808
GitHub Action <action@github.com>
2021-01-30 09:11:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/misjaalma/livyideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/misjaalma/livyideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.misjaalma.livyideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.misjaalma.livyideaplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/misjaalma/livyideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/misjaalma/livyideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.misjaalma.livyideaplugin.services
 
+import com.github.misjaalma.livyideaplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
